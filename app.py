5c29792a67165c0016ea05c8b2142e817f22ebd1
sk9362 <sk9362@gmail.com>
2023-10-09 21:03:47 +0530

    adding dockerfile and app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def main():
+    return "Welcome!"
+@app.route('/how are you')
+def hello():
+    return 'I am agood, how about you'
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
